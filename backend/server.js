f0a530865f5bb04239cd25ae8f5895a1c9157794
srikar122 <93542629+srikar122@users.noreply.github.com>
2025-01-04 21:23:27 +0530

    3rd

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use('/api/cart', cartRouter)
 app.use('/api/order',orderRouter)
 
 app.get('/', (req,res)=>{
-    res.send('Hello World')
+    res.send('api is working')
 })
 
 app.get('/favico.ico' , function(req , res){/*code*/});
